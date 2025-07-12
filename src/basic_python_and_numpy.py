2167ecfd7ae5096719793430d596c257ae4ec20d
Mohammad Fallah <mmohammadmfallahm02@gmail.com>
2025-07-13 02:16:30 +0330

    feat: movie-ratings-analysis Filter movies by rating threshold and specific year or genre.
    #t-1.2

--- src/basic_python_and_numpy.py
+++ src/basic_python_and_numpy.py
@@ -10,15 +10,35 @@ with open(file_path, 'r') as csvfile:
 
 data_array = np.array(data)
 
-# header = data_array[0]
-# print('Header: ', header)
+header = np.array(data[0])          # First row is the header
+movies = np.array(data[1:])          # The rest is the movie data
 
 
-rating_array = data_array[1:, 6].astype(float)
+def calculate_statistics_rating():
+    rating_array = movies[:, 6].astype(float)
 
-# print('Rating array: ', rating_array)
-print('Mean:', np.mean(rating_array))
-print('Standard deviation:', np.std(rating_array))
-print('Median:', np.median(rating_array))
+    print('Mean:', np.mean(rating_array))
+    print('Standard deviation:', np.std(rating_array))
+    print('Median:', np.median(rating_array))
+
+def filtered_movie_by_imdb_rating(rating):
+    rate_filter = (movies[:, 6]).astype(float) > rating
+    filtered_movie = movies[rate_filter]
+
+    print('Filtered by IMDB rating:', filtered_movie)
+
+
+def filtered_movie_by_year(year):
+    year_filter = movies[:, 2].astype(str) == year
+    filtered_movie = movies[year_filter]
+
+    print('Filtered by year:', filtered_movie)
+
+
+def filtered_movie_by_genre(genre):
+    genre_filter = np.array([genre in g for g in movies[:, 5]])
+    filtered_movie = movies[genre_filter]
+
+    print('Filtered by genre:', filtered_movie)
 
 
